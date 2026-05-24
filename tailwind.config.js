db2dd52ecd1bbf0b4c59dae95959fdbbaac972e1
Afeef <contact@afeef.dev>
2026-05-24 11:45:31 +0800

    feat: updated wedding bg colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
-        'wedding-bg': '#5b0000',
+        'wedding-bg': '#2b1511',
         'card-olive': '#4d5530',
         'card-brown': '#443511',
         'card-mustard': '#cd9e3f',
@@ -18,7 +16,7 @@ export default {
       fontFamily: {
         display: ['var(--font-display)'],
         sans: ['var(--font-sans)'],
-      }
+      },
     },
   },
   plugins: [],
