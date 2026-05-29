afc68be0d5669745444fbceba636023cc85ac18d
Afeef <contact@afeef.dev>
2026-05-29 10:45:11 +0800

    fix: removed redundant main

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,9 +2,5 @@ import React from 'react'
 import { LandingPage } from '@/components/LandingPage'
 
 export default function HomePage() {
-  return (
-    <main>
-      <LandingPage />
-    </main>
-  )
+  return <LandingPage />
 }
