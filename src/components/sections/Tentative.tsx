87fc5b66f0fa63078ead5cef22ecb0b96d3b7dbe
Afeef <contact@afeef.dev>
2026-05-29 10:44:21 +0800

    feat: updated tentative

--- src/components/sections/Tentative.tsx
+++ src/components/sections/Tentative.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image'
 
 export default function Tentative() {
   return (
-    <div className="flex flex-col gap-6 h-full overflow-y-auto pb-4">
+    <div className="flex flex-col gap-6 h-full overflow-y-auto scrollbar-none pb-4 text-4xl sm:text-7xl tracking-wide font-light">
       {/* Schedule overview image */}
       <div className="rounded-2xl">
         <Image
-          src="/image/tentative-v2.png"
+          src="/image/tentative-v3.png"
           alt="Wedding day schedule overview"
           width={800}
           height={600}
