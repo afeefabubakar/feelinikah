3cae33fac71267d5b9b42647cef887c8d242b236
Afeef <contact@afeef.dev>
2026-05-30 21:03:29 +0800

    feat: updated about us copywriting

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -12,18 +12,19 @@ export default function About() {
         <p className="italic text-white/60 border-l-2 border-white/20 pl-4 mb-4 text-xl"></p>
         <p>
           We were separated by the ocean and 987km away for 10,501 days before our eyes first met.
-          Allah SWT said what is meant for us, will find us.
         </p>
         <p>Facts (some maybe fun):</p>
         <ul className="list-disc ml-8">
           <li>Alin is 10 moons wiser than Afeef.</li>
           <li>Alin is more outdoorsy and Afeef is more homebody.</li>
-          <li>Alin loves her personal space and Afeef also loves Alin's personal space.</li>
           <li>Alin is a deep thinker and Afeef is a what-makes-sense-for-me thinker.</li>
-          <li>Alin eats more vegetables than Afeef.</li>
+          <li>Alin loves her personal space and Afeef also loves Alin's personal space.</li>
           <li>
             Alin knows how to navigate underwater, and doesn’t need to use use Waze when driving
             while Afeef is always so impressed by that.
+          </li>
+          <li>
+            Alin is more operational-business minded and Afeef is more software-developing minded.
           </li>
         </ul>
         <p>
