50e63516663544b6efa649e8bc4deeaa1f6d08b1
Afeef <contact@afeef.dev>
2026-06-01 18:23:11 +0700

    feat(migration): add description field to wishlist collection

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260529_065832_init from './20260529_065832_init';
+import * as migration_20260601_112219_add_wishlist_description from './20260601_112219_add_wishlist_description';
 
 export const migrations = [
   {
     up: migration_20260529_065832_init.up,
     down: migration_20260529_065832_init.down,
-    name: '20260529_065832_init'
+    name: '20260529_065832_init',
+  },
+  {
+    up: migration_20260601_112219_add_wishlist_description.up,
+    down: migration_20260601_112219_add_wishlist_description.down,
+    name: '20260601_112219_add_wishlist_description'
   },
 ];
